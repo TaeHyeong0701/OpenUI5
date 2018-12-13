c690114bd76d6fc47183f81a2d46a02ea0b9163f
TaeHyoung Kim <xdkvlago@gmail.com>
2018-12-13 13:23:08 +0900

    13 December CO03 additional material add

---
+++ CO03/Component.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	   "sap/ui/core/UIComponent",
+	   "sap/ui/model/json/JSONModel"
+	], function (UIComponent, JSONModel, ResourceModel) {
+	   "use strict";
+	   return UIComponent.extend("THproject.Component", {  //UI Component를 사용하겠다.
+	      metadata : {
+	    	  manifest : "json"   // JSON 호출
+	      }, 
+	   });
+	});
