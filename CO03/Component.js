393afb9ca656cbcc725f4315ca33e503d7c286a4
TaeHyoung Kim <xdkvlago@gmail.com>
2018-12-13 18:42:33 +0900

    13 December Update

--- CO03/Component.js
+++ CO03/Component.js
@@ -6,6 +6,6 @@ sap.ui.define([
 	   return UIComponent.extend("THproject.Component", {  //UI Component를 사용하겠다.
 	      metadata : {
 	    	  manifest : "json"   // JSON 호출
-	      }, 
+	      } 
 	   });
 	});
