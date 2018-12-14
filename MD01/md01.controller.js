1dd985112f76405a26f25328818f1aade44ad8a6
TaeHyoung Kim <xdkvlago@gmail.com>
2018-12-14 16:04:59 +0900

    14 December add

--- MD01/md01.controller.js
+++ MD01/md01.controller.js
@@ -7,10 +7,30 @@ sap.ui.define([
 	   "use strict";
 	   return Controller.extend("THproject.controller.md01", {   // Project ---> Controller ---> name으로 반환한다.
 
+//		     onInit : function () {
+//		         // set data model on view
+//		    	 oSplitApp.attachMasterButton(function(event) {
+//		    		    if (event.getParameter("show")) { 
+//		    		        let button = this.byId(this.createId("mySplitApp-MasterBtn"));
+//		    		        if (button) {
+//		    		            button.setIcon("sap-icon://filter");
+//		    		        }
+//		    		    }
+//		    		}, this);
+//		      }
 		   onSysenter : function()
 		   {
 			   alert("이것은 Enter Button 입니다. ");
 		   }
 
+
+//	      onShowHello : function () {    // read msg from i18n model
+//	          var oBundle = this.getView().getModel("i18n").getResourceBundle(); // 모델을 가져와서 화면으로 던져준다.
+//	          var sRecipient = this.getView().getModel().getProperty("/recipient/name");  // 모델에서 getProperty로 데이터를 꺼낸다.
+//	          var sMsg = oBundle.getText("helloMsg", [sRecipient]);  // helloMsg를 getText해서 텍스트 값을 꺼낸다. 배열에 값을 꺼낸다.
+//	          // show message
+//	          MessageToast.show(sMsg);
+//	         MessageToast.show("Hello World");
+//	      }
 	   });
 	});
